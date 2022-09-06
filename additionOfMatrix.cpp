5b794400a9eb25473933eac82406013c5b4683fc
Gonthini Bhaskar <49478324+bhaskargonthini@users.noreply.github.com>
2022-09-06 09:44:52 +0530

    Create additionOfMatrix.cpp

---
+++ additionOfMatrix.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution
+{   
+    public:
+    //Function to add two matrices.
+    vector<vector<int> > sumMatrix( const vector<vector<int> >& A, const vector<vector<int> >& B)
+    {
+        // code here
+        vector<vector<int>> res(A.size());
+        if(A.size()!=B.size() or A[0].size()!=B[0].size())
+        return {};
+        int col = A[0].size();
+        for(int i=0;i<res.size();i++){
+            res[i].assign(col,0);
+            for(int j=0;j<col;j++){
+                res[i][j] = A[i][j]+B[i][j];
+            }
+        }
+        return res;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > A(row); 
+        for(int i=0; i<row; i++)
+        {
+            A[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>A[i][j];
+            }
+        }
+        cin>> row>> col;
+        vector<vector<int> > B(row);
+        for(int i=0; i<row; i++)
+        {
+            B[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>B[i][j];
+            }
+        }
+        Solution ob;
+        vector<vector<int> > result = ob.sumMatrix(A,B);
+        if(result.size() == 0)
+            cout<<"-1";
+        else
+        {
+            for (int i = 0; i < result.size(); ++i)
+            {
+                for (int j = 0; j < result[0].size(); ++j)
+                {
+                    cout<<result[i][j]<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
